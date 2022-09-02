22c9272f09cd8132cdb3485e8bb6cf3a284b67b0
vaibhavsen7 <110538806+VaibhavGlSen@users.noreply.github.com>
2022-09-02 13:17:12 +0530

    CAPSTONE PROJECT

---
+++ HotelBookingManagementSystem/src/test/java/com/Team4/project/HotelBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Team4.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
